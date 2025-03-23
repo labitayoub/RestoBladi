59f25f0db98b2c871780b05308bf48790be01bed
labitayoub <ayoub.labite@gmail.com>
2025-03-23 23:59:39 +0000

    update model Sale

--- app/Models/Sale.php
+++ app/Models/Sale.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Sale extends Model
 {
     use HasFactory;
+    protected $fillable = ['total_ht', 'tva', 'total_ttc', 'payment_type', 'waiter_id'];
+
+    public function waiter()
+    {
+        return $this->belongsTo(Waiter::class);
+    }
+    public function menus()
+    {
+        return $this->belongsToMany(Menu::class);
+    }
+    public function tables()
+    {
+        return $this->belongsToMany(Table::class);
+    }
 }
