efc64c3c4c053ee661dfbc3b8a5232910daaca3a
labitayoub <ayoub.labite@gmail.com>
2025-04-05 16:27:39 +0000

    update menus controller

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Menu;
+use App\Models\Category;
 use App\Http\Requests\StoreMenuRequest;
 use App\Http\Requests\UpdateMenuRequest;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
 
 class MenuController extends Controller
 {
@@ -28,7 +31,9 @@ class MenuController extends Controller
      */
     public function create()
     {
-        //
+        return view("manager.gestion.menus.create")->with([
+            "categories" => Category::all()
+        ]);
     }
 
     /**
@@ -37,15 +42,41 @@ class MenuController extends Controller
      * @param  \App\Http\Requests\StoreMenuRequest  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(StoreMenuRequest $request)
+    public function store(Request $request)
     {
-        //
+        //validation
+        $this->validate($request, [
+            "title" => "required|min:3|unique:menus,title",
+            "description" => "required|min:5",
+            "image" => "required|image|mimes:png,jpg,jpeg|max:2048",
+            "price" => "required|numeric",
+            "category_id" => "required|numeric",
+        ]);
+        //store data
+        if ($request->hasFile("image")) {
+            $file = $request->image;
+            $imageName = time() . "_" . $file->getClientOriginalName();
+            $file->move(public_path('images/menus'), $imageName);
+            $title = $request->title;
+            Menu::create([
+                "title" => $title,
+                "slug" => Str::slug($title),
+                "description" =>  $request->description,
+                "price" =>  $request->price,
+                "category_id" =>  $request->category_id,
+                "image" =>  $imageName,
+            ]);
+            //redirect user
+            return redirect()->route("menus.index")->with([
+                "success" => "menu ajouté avec succés"
+            ]);
+        }
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Menu  $menu
+     * @param  \App\Menu  $menu
      * @return \Illuminate\Http\Response
      */
     public function show(Menu $menu)
@@ -56,34 +87,85 @@ class MenuController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\Menu  $menu
+     * @param  \App\Menu  $menu
      * @return \Illuminate\Http\Response
      */
     public function edit(Menu $menu)
     {
         //
+        return view("managments.menus.edit")->with([
+            "categories" => Category::all(),
+            "menu" => $menu
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \App\Http\Requests\UpdateMenuRequest  $request
-     * @param  \App\Models\Menu  $menu
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Menu  $menu
      * @return \Illuminate\Http\Response
      */
-    public function update(UpdateMenuRequest $request, Menu $menu)
+    public function update(Request $request, Menu $menu)
     {
         //
+        //validation
+        $this->validate($request, [
+            "title" => "required|min:3|unique:menus,title," . $menu->id,
+            "description" => "required|min:5",
+            "image" => "image|mimes:png,jpg,jpeg|max:2048",
+            "price" => "required|numeric",
+            "category_id" => "required|numeric",
+        ]);
+        //store data
+        if ($request->hasFile("image")) {
+            unlink(public_path('images/menus/' . $menu->image));
+            $file = $request->image;
+            $imageName = time() . "_" . $file->getClientOriginalName();
+            $file->move(public_path('images/menus'), $imageName);
+            $title = $request->title;
+            Menu::create([
+                "title" => $title,
+                "slug" => Str::slug($title),
+                "description" =>  $request->description,
+                "price" =>  $request->price,
+                "category_id" =>  $request->category_id,
+                "image" =>  $imageName,
+            ]);
+            //redirect user
+            return redirect()->route("menus.index")->with([
+                "success" => "menu modifié avec succés"
+            ]);
+        } else {
+            $title = $request->title;
+            $menu->update([
+                "title" => $title,
+                "slug" => Str::slug($title),
+                "description" =>  $request->description,
+                "price" =>  $request->price,
+                "category_id" =>  $request->category_id
+            ]);
+            //redirect user
+            return redirect()->route("menus.index")->with([
+                "success" => "menu modifié avec succés"
+            ]);
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Menu  $menu
+     * @param  \App\Menu  $menu
      * @return \Illuminate\Http\Response
      */
     public function destroy(Menu $menu)
     {
-        //
+        //remove image
+        unlink(public_path('images/menus/' . $menu->image));
+        $menu->delete();
+        //redirect user
+        return redirect()->route("menus.index")->with([
+            "success" => "menu supprimé avec succés"
+        ]);
     }
 }
