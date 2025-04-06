a35db9f4bcd85a220e87ce456365796aa97672e2
labitayoub <ayoub.labite@gmail.com>
2025-04-06 01:19:56 +0000

    update  OrderController

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Models\Table;
 
 class OrderController extends Controller
 {
     public function index()
     {
-        $
+        return view("waiter.order")->with([
+            "tables" => Table::all(),
+            "categories" => Category::all(),
+        ]);
 }
