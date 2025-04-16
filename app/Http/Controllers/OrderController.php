b700465a5cc27ee35810411fc6016126ca5b8c7f
labitayoub <ayoub.labite@gmail.com>
2025-04-16 20:43:25 +0100

    update of order page

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -7,6 +7,7 @@ use App\Models\Category;
 use App\Models\Table;
 use App\Models\Waiter;
 use App\Models\Sale;
+use App\Models\Menu;
 use Illuminate\Support\Facades\Auth;
 
 class OrderController extends Controller
@@ -20,12 +21,22 @@ class OrderController extends Controller
             return redirect()->route('dashboard')->with('error', 'Profil serveur non trouvé.');
         }
         
+        $waiterId = $waiter->id;
         $manager_id = $waiter->manager_id;
         
+        $todayOrders = Sale::where('waiter_id', $waiterId)
+        ->whereDate('created_at', today())
+        ->count() ?? 0;
+        
+        // Calculer les ventes du jour pour ce serveur (montant total)
+        $todaySales = Sale::where('waiter_id', $waiterId)
+        ->whereDate('created_at', today())
+        ->sum('total_ttc') ?? 0;
+
         // Récupérer les tables et catégories du manager
         $tables = Table::where('manager_id', $manager_id)->get();
-        $categories = Category::where('manager_id', $manager_id)->get();
+        $categories = Category::where('manager_id', $manager_id)->with('menus')->get();
         
-        return view("waiter.orders.index", compact('tables', 'categories'));
+        return view("waiter.orders.index", compact('tables', 'categories', 'todayOrders', 'todaySales'));
     }
 }
